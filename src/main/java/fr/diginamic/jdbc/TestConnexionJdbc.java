12ef4f0c3525f6aa814a23909000c5feafed9362
FYHenry <frederic_yann.henry@protonmail.com>
2023-06-12 17:15:50 +0200

    TP2 done

--- src/main/java/fr/diginamic/jdbc/TestConnexionJdbc.java
+++ src/main/java/fr/diginamic/jdbc/TestConnexionJdbc.java
@@ -2,15 +2,28 @@ package fr.diginamic.jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
+import java.util.ResourceBundle;
 
 public class TestConnexionJdbc {
-    private static final String unix_socket = "jdbc:mariadb://localhost/";
-    private static final String ip_port = "jdbc:mariadb://127.0.0.1:3306/";
+    private static final String unix_socket;
+    private static final String user;
+    static {
+        final ResourceBundle rb = ResourceBundle.getBundle("database");
+        unix_socket = rb.getString("ADDON_SCHEME") +
+                rb.getString("ADDON_HOST") +
+                rb.getString("ADDON_DB");
+        user = rb.getString("ADDON_USER");
+    }
     public static void main(String[] args) throws SQLException{
-            try(Connection connection = DriverManager.getConnection(unix_socket + "compta",
-                    "root",
-                    "temporaire")){
-            System.out.println("Ça marche !");
+        System.out.println("Mot de passe :");
+        Scanner scanner = new Scanner(System.in);
+        final String pwd = scanner.nextLine();
+        try(Connection connection =
+                    DriverManager.getConnection(unix_socket,
+                            user,
+                    pwd)){
+                System.out.println("Ça marche !");
                 System.out.println(connection);
         }
     }
